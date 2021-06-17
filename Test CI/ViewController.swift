043576011dca28dc11325bf90dfcc67837a2df0f
Graham McConnell <3437644+graham-mc@users.noreply.github.com>
2021-06-17 16:16:48 +0100

    Initial Commit

---
+++ Test CI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test CI
+//
+//  Created by Graham McConnell on 17/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test CITests/Test_CITests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_CITests.swift
+//  Test CITests
+//
+//  Created by Graham McConnell on 17/06/2021.
+//
+
+import XCTest
+@testable import Test_CI
+
+class Test_CITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
